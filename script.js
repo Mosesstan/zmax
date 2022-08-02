2f7be6f05f9cae66c03fd0c86c8f721c59114719
Mosesstan <88045334+Mosesstan@users.noreply.github.com>
2022-08-02 12:55:26 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 function myFunction() {
   var x = document.getElementById("myTopnav");
   if (x.className === "topnav") {
@@ -5,4 +6,26 @@ function myFunction() {
   } else {
     x.className = "topnav";
   }
+}
+//slideshow
+
+
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}    
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+  setTimeout(showSlides, 3000); // Change image every 2 seconds
 }
